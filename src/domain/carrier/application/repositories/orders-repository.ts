f56975c4e049288566d4fe46f435985a0410db16
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2025-06-11 23:18:24 -0300

    refactor: update OrdersRepository to include additional abstract methods for order management

--- src/domain/carrier/application/repositories/orders-repository.ts
+++ src/domain/carrier/application/repositories/orders-repository.ts
@@ -1,6 +1,12 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
-import { Order } from '../../enterprise/entities/order'
+import { Order } from '@/domain/carrier/enterprise/entities/order'
+import { OrderDetails } from '@/domain/carrier/enterprise/entities/value-objects/order-details'
 
 export abstract class OrdersRepository {
+  abstract findById(id: string): Promise<Order | null>
+  abstract findByIdWithDetails(id: string): Promise<OrderDetails | null>
+  abstract findManyRecent(params: PaginationParams): Promise<Order[]>
+  abstract save(order: Order): Promise<void>
   abstract create(order: Order): Promise<void>
+  abstract delete(order: Order): Promise<void>
 }
